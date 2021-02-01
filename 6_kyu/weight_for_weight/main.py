9021a58262ab818453fe1dfba4b6097f2ba0e789
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-01 17:56:41 +0530

    todo: solve weight for weights

--- 6_kyu/weight_for_weight/main.py
+++ 6_kyu/weight_for_weight/main.py
@@ -4,10 +4,22 @@ def order_weight(strng):
     def sumOfDigits(n):
         digits = list(map(int, n.strip()))
         return sum(digits)
-    
+
     weights.sort(key=sumOfDigits)
 
+    for i in range(len(weights)):
+        if i < (len(weights)-1):
+            if (sumOfDigits(weights[i]) == sumOfDigits(weights[i+1])):
+                if (weights[i][0] > weights[i+1][0]):
+                    print(weights[i], weights[i+1])
+                    temp = weights[i]
+                    weights[i] = weights[i+1]
+                    weights[i+1] = temp
+                    
     return ' '.join(weights)
+
+
+
 
 
 
@@ -15,4 +27,4 @@ def order_weight(strng):
 
 print(order_weight("103 123 4444 99 2000"), "2000 103 123 4444 99")
 print(order_weight("2000 10003 1234000 44444444 9999 11 11 22 123"), "11 11 2000 10003 22 123 1234000 44444444 9999")
-print(order_weight(""), "")
+# print(order_weight(""), "")
