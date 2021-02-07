893bf173795501bacb18e432642a0614ee582a1a
Mir Misbah Ali <mirmisbahali@gmail.com>
2021-02-07 22:22:24 +0530

    added best solution

--- 6_kyu/sum_of_digits/main.py
+++ 6_kyu/sum_of_digits/main.py
@@ -1,3 +1,4 @@
+# My Solution
 def digital_root(n):
     sum_of_digits = sum(int(number) for number in str(n))
     
@@ -6,6 +7,10 @@ def digital_root(n):
     
     return sum_of_digits
 
+
+# Best Soluton
+# def digital_root(n):
+#     return n%9 or n and 9 
 
 
 print(digital_root(16), 7)
